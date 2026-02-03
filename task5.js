a56096607a8e373c445142a68b1a3fb26b24fe32
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-03 06:53:34 +0600

    Complete task5

---
+++ task5.js
@@ -0,0 +1,7 @@
+/**
+ * Generate a random number between 10 to 20
+ */
+
+let randomNumber = Math.random()*(20-10)+10;
+let limitRandomNumber = Math.round(randomNumber);
+console.log(limitRandomNumber);
